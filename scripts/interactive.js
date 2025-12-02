dbd47437b1ec4c5986cfc5fc672369e4cd0f4dd3
DubeykoVT <dubeykovt@gmail.com>
2025-12-02 20:27:59 +0300

    Добавление ДО

--- scripts/interactive.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('.toggle').forEach(btn=>{
-  btn.onclick = ()=>{
-    let c = btn.nextElementSibling;
-    c.style.display = c.style.display==='block'?'none':'block';
-  };
-});
