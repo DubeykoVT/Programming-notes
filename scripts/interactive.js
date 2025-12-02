cde010324a879ff24a1ab842907c8cbf86049fa0
DubeykoVT <dubeykovt@gmail.com>
2025-12-02 20:17:54 +0300

    Добавление ДО

--- scripts/interactive.js
+++ scripts/interactive.js
@@ -1,1 +1,7 @@
-console.log('interactive loaded');
+
+document.querySelectorAll('.toggle').forEach(btn=>{
+  btn.onclick = ()=>{
+    let c = btn.nextElementSibling;
+    c.style.display = c.style.display==='block'?'none':'block';
+  };
+});
